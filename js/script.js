609a5e7337aa6c885cecef611cbb3287192f0e87
Nuzze <mati280341@gmail.com>
2019-11-10 04:27:03 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Cuenta regresiva
+
+var countDownDate = new Date("Dec 3, 2019 21:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "ANDÁ A PLAZA MALVINAS";
+  }
+}, 1000);
